527387a5b879825f4bb6940c03c1fd622c9c8fea
pranali <kokatepranu833@gmail.com>
2025-11-19 21:58:36 +0530

    Create Contact.jsx
    new code added

---
+++ assignment/src/Contact.jsx
@@ -0,0 +1,12 @@
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <div className="contact-container">
+      <h1 className="contact-title">Contact Page</h1>
+      <p className="contact-text">Contact us at support@example.com</p>
+    </div>
+  );
+}
+
+export default Contact;
