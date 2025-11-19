27b93c6a240667841831899a275c5bcf6c69850f
pranali <kokatepranu833@gmail.com>
2025-11-19 21:55:24 +0530

    Create Home.jsx
    code added

---
+++ assignment/src/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Home Page</h1>
+      <p className="home-text">Welcome to the React Router Assignment!</p>
+    </div>
+  );
+}
+
+export default Home;
