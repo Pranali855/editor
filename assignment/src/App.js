89e705df296264cf19b192077f250e8f36f877cd
pranali <kokatepranu833@gmail.com>
2025-12-10 19:16:17 +0530

    Update App.js
    initial commit

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,24 +1,17 @@
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Contact from "./Contact";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./assets/Home";
+import About from "./assets/About";
+import Contact from "./assets/Contact";
 
 function App() {
   return (
-    <div>
-      <nav className="navbar">
-        <Link className="nav-link" to="/">Home</Link>
-        <Link className="nav-link" to="/about">About</Link>
-        <Link className="nav-link" to="/contact">Contact</Link>
-      </nav>
-
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
