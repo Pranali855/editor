531ea7c2f2a81f0a75d6f9c0e63ed9d0f7f62d68
pranali <kokatepranu833@gmail.com>
2025-12-21 18:59:16 +0530

    Create Contact.jsx
    basic changes added

---
+++ assignment/src/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Contact.css";
+import Navbar from "./Navbar";
+
+function Contact() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>Contact Us</h1>
+        <p className="diwali">Stay safe</p>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
